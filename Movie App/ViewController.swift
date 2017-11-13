45cf8d085a5b9135542f8ff6b28d9d59e415f05d
Богдан Семенюк <bogdansemenuk@MacBook-Pro-Bogdan.local>
2017-11-13 22:00:47 +0200

    set up gitignore

---
+++ Movie App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Movie App
+//
+//  Created by Богдан Семенюк on 13.11.2017.
+//  Copyright © 2017 Bogdan Semeniuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Movie App' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Movie App
+pod 'Moya', '~> 10.0'
+pod 'KeychainSwift', '~> 10.0'
+
+end
