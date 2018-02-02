cd2b65cac60d2e313b17f5d49f780aca194f6685
Bogdan <000sbv000@gmail.com>
2018-02-02 20:46:36 +0200

    Added search text field

--- Movie App/ViewControllers/SearchViewController.swift
+++ Movie App/ViewControllers/SearchViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    lazy var searchTextField: UITextField = {
+        let frame = getFrameOfSearchTextField()
+        let textField = UITextField(frame: frame)
+        textField.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationItem.title = "HAHAHA"
+        
+        navigationItem.titleView = searchTextField
+        
+    }
+    
+    func getFrameOfSearchTextField() -> CGRect {
+        if let navBarBounds = navigationController?.navigationBar.bounds.size {
+            let point = CGPoint(x: 0.0, y: 0.0)
+            let height = navBarBounds.height - 12.0
+            let wight = navBarBounds.width - 150.0
+            return CGRect(origin: point, size: CGSize(width: wight, height: height))
+        }
+        return CGRect(x: 0, y: 0, width: 0, height: 0)
     }
     
     static func create() -> UIViewController {
